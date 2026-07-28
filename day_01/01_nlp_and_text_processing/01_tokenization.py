747e20f0300f85c5b370b3a917060a9009a63cda
Ruthran Raghavan <ruthran.raghavan@tellestia.com>
2026-07-28 11:43:05 +0530

    update: 2026-07-28 11:43:05

--- day_01/01_nlp_and_text_processing/01_tokenization.py
+++ day_01/01_nlp_and_text_processing/01_tokenization.py
@@ -19,3 +19,33 @@
 import nltk
 from nltk.tokenize import word_tokenize, sent_tokenize
 import spacy
+
+nltk.download("punkt", quiet=True)
+nltk.download("punkt_tab", quiet=True)
+
+nlp = spacy.load("en_core_web_sm")
+
+sentence = "Mandy didn't pay the EMI of Rs.45,000 on 3rd May; the bank charged 3.5% interest."
+
+# Way 1: Using naive split (split by space)
+print("\n 1. Navie Split (split by space):")
+naive = sentence.split()
+print(naive)
+print("count:", len(naive))
+
+# Way 2: Using NLTK tokenizer
+print("\n 2. NLTK Tokenizer:")
+nltk_tokens = word_tokenize(sentence)
+print(nltk_tokens)
+print("count:", len(nltk_tokens))
+
+# Way 3: Using spaCy tokenizer
+print("\n 3. spaCy Tokenizer:")
+doc = nlp(sentence)
+print([token.text for token in doc])
+print("count:", len(doc))
+
+print("\n piece-by-piece comparison of tokens:")
+print(f" {'Token':<12} {'IS IT A WORD?':<15} {"IS IT PUNCTUATION?":<20} {'IS IT A NUMBER?':<20}")
+for token in doc:
+    print(f" {token.text:<12} {str(token.is_alpha):<15} {str(token.is_punct):<20} {token.like_num:<20}")

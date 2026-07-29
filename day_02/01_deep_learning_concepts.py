f9a0b4018674fba259817cf55459ae6fc59e3483
Ruthran Raghavan <ruthran.raghavan@tellestia.com>
2026-07-29 10:32:49 +0530

    update: 2026-07-29 10:32:49

---
+++ day_02/01_deep_learning_concepts.py
@@ -0,0 +1,13 @@
+import os
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"  # Suppress TensorFlow logging (1)
+os.environ["TF_ENABLE_ONEDNN_OPTS"] = "0"  # Disable oneDNN to avoid AVX/FMA warnings
+
+import warnings
+warnings.filterwarnings("ignore")  # Suppress warnings
+
+import absl.logging as absl_logging
+absl_logging.set_verbosity(absl_logging.ERROR)  # Suppress absl logging
+
+import numpy as np
+import tensorflow as tf
+

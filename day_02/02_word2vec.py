5ae3d28a730e276d0033bc1fa22e7e5aff9fcb0f
Ruthran Raghavan <ruthran.raghavan@tellestia.com>
2026-07-29 12:40:06 +0530

    update: 2026-07-29 12:40:06

--- day_02/02_word2vec.py
+++ day_02/02_word2vec.py
@@ -7,3 +7,9 @@ import numpy as np
 from dotenv import load_dotenv
 from openai import OpenAI
 from sklearn.decomposition import PCA
+
+load_dotenv()
+
+client = OpenAI(
+    api_key=os.environ["OPENROUTER_API_KEY"],
+    base_url=os.environ["OPENROUTER_BASE_URL"],
